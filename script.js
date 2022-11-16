b51a43b969162666bf704edb74e1403b052d1fe8
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-11-16 20:35:44 -0300

    Jogando no github

---
+++ script.js
@@ -0,0 +1,86 @@
+/*
+1. Crie uma classe Carro com as seguintes propriedades:
+● Nome - qual a equipe do carro Ferrari, Mercedes, McLaren, etc;
+● Potência - número de cavalos de potência do carro;
+● Velocidade Máxima - qual a maior velocidade que o carro pode alcançar;
+● Aceleração - o tempo em segundos que o carro leva para ir de 0 a 100 km/h
+2. Adicione na classe carro um método que recebe como parâmetro uma distância em
+metros e calcula o tempo em segundos para percorrer essa distância. Use a
+seguinte fórmula para o cálculo: resultado = distância / (Velocidade Máxima /
+Aceleração). A função deve retornar esse resultado.
+3. Crie alguns objetos da classe Carro e adicione-os em um array
+4. Crie uma classe Corrida com as seguintes propriedades:
+● Nome - nome do local da corrida;
+● Tipo - uma corrida pode ser Fórmula 1, Stock Car, Rally, etc;
+● Distância - o total em metros da corrida;
+● Vencedor - qual a equipe que ganhou a corrida;
+5. Adicione na classe Corrida um método que verifica qual foi o carro que completou a
+corrida em menor tempo, para isso utilize o método criado na classe carro. Salve o
+nome da equipe que fez o menor tempo na propriedade “Vencedor”.
+6. Adicione na classe Corrida um método que exibe na tela quem é o vencedor da
+corrida.
+7. Crie um objeto da classe Corrida e chame seus métodos.
+*/
+
+class Carro{
+    NomeEquipe = []
+    Potencia = []
+    VelocidadeMaxima = []
+    Aceleracao = []
+
+    MetrosSegundos(distancia){
+        let MetrosSegundos = []
+        for (let index = 0; index < 3; index++) {
+                MetrosSegundos[index] = distancia / (this.VelocidadeMaxima[index] / this.Aceleracao[index])
+        }
+
+        return MetrosSegundos;
+    }
+}
+
+class Corrida{
+    NomeLocal
+    TipoCorrida
+    DistanciaCorrida
+    VencedorCorrida
+
+    VerificarVencedor(VencedorCorrida){
+        let min = Math.min(...VencedorCorrida)
+        console.log(min)
+        let posicaoGanhador = VencedorCorrida.indexOf(min)
+        console.log(posicaoGanhador)
+        return posicaoGanhador;
+    }
+
+    ExibirGanhador(posicaoGanhador, carro){
+        this.VencedorCorrida = carro.NomeEquipe[posicaoGanhador]
+        alert("o vencedor da corrida é a equipe: " + this.VencedorCorrida)
+    }
+
+}
+
+let corrida1 = new Corrida()
+corrida1.DistanciaCorrida = parseInt(prompt("Qual a distância da corrida?"))
+corrida1.NomeLocal = "Veneza"
+corrida1.TipoCorrida = "Formula 1"
+
+let carro = new Carro()
+carro.NomeEquipe[0] = "ferrari"
+carro.Potencia[0] = 450
+carro.VelocidadeMaxima[0] = 390
+carro.Aceleracao[0] = 6
+
+carro.NomeEquipe[1] = "Mercedes"
+carro.Potencia[1] = 400
+carro.VelocidadeMaxima[1] = 2000
+carro.Aceleracao[1] = 1
+
+carro.NomeEquipe[2] = "Uno"
+carro.Potencia[2] = 420
+carro.VelocidadeMaxima[2] = 450
+carro.Aceleracao[2] = 4
+
+let retorno = carro.MetrosSegundos(corrida1.DistanciaCorrida)
+
+let posicaoVencedor = corrida1.VerificarVencedor(retorno)
+corrida1.ExibirGanhador(posicaoVencedor, carro)
